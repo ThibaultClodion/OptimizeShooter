27e065636987776a060be9d5b78f04c0c52ec1f3
Thibault Clodion <thibaultclodion@gmail.com>
2025-11-08 17:22:57 +0100

    Add Enhanced Input on Soldier + Movement

--- Source/OptimizeShooter/Private/Characters/Soldier.cpp
+++ Source/OptimizeShooter/Private/Characters/Soldier.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Characters/Soldier.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 ASoldier::ASoldier()
 {
@@ -10,7 +12,9 @@ ASoldier::ASoldier()
 
 void ASoldier::BeginPlay()
 {
-	Super::BeginPlay();	
+	Super::BeginPlay();
+
+	InitializeMappingContext();
 }
 
 void ASoldier::Tick(float DeltaTime)
@@ -21,5 +25,39 @@ void ASoldier::Tick(float DeltaTime)
 void ASoldier::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASoldier::Move);
+	}
+}
+
+void ASoldier::InitializeMappingContext()
+{
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+
+	if (PlayerController)
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(CharacterMappingContext, 0);
+		}
+	}
+}
+
+void ASoldier::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	UE_LOG(LogTemp, Warning, TEXT("Movement Vector: X=%f, Y=%f"), MovementVector.X, MovementVector.Y);
+
+	if (Controller)
+	{
+		const FVector ForwardDirection = GetActorForwardVector();
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+
+		const FVector RightDirection = GetActorRightVector();
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
 }
 
--- Source/OptimizeShooter/Public/Characters/Soldier.h
+++ Source/OptimizeShooter/Public/Characters/Soldier.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "Soldier.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class OPTIMIZESHOOTER_API ASoldier : public ACharacter
@@ -15,8 +19,20 @@ public:
 	ASoldier();
 
 	virtual void Tick(float DeltaTime) override;
+
+	/** Inputs **/
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void Move(const FInputActionValue& Value);
 
 protected:
 	virtual void BeginPlay() override;
+
+	void InitializeMappingContext();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* CharacterMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+
 };
